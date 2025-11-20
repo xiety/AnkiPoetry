bd10a37e9899bd307c6ed4c14a0519323702306a
xiety <xiety666@gmail.com>
2025-11-20 19:37:28 +0200

    .net10

--- AnkiPoetry.BlazorWasm/Program.cs
+++ AnkiPoetry.BlazorWasm/Program.cs
@@ -9,7 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
